ab657444a469d4877bc004d69d549e15b25b201a
Arthur201 <arthurlaureanosilva2001@gmailcom>
2018-07-27 10:41:17 -0300

    Double jmup ATIVADO

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -37,6 +37,10 @@ public class PlayerCtrl : MonoBehaviour {
 	public bool isGrounded;
 	public LayerMask whatIsGround;
 
+
+	bool canDoubleJump = false;
+	public float delayForDoubleJump = 0.2f;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
@@ -109,9 +113,23 @@ public class PlayerCtrl : MonoBehaviour {
 		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpspeed));
 		anim.SetInteger("State", 1);
-		}	
 
-	}	
+		invoke("EnableDoubleJump", delayForDoubleJump);
+
+		}	
+		if(canDoubleJump && !isGrounded){
+		rb.velocity = Vector2.zero;	
+		rb.AddForce(new Vector2(0f, jumpspeed));
+		anim.SetInteger("State", 1);
+		canDoubleJump = false;
+		}
+
+	}
+
+	void EnableDoubleJump(){
+		canDoubleJump = true;
+	}
+	
 
 	void OnCollisionEnter2D(Collision2D other){
 		if(other.gameObject.layer == LayerMask.NameToLayer("Ground")){
